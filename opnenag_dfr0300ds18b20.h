b052933520352aae57ba311d754aef3feb7e28da
KhalladB <eng.khallad@hotmail.com>
2016-12-01 12:06:41 +0300

    Update opnenag_dfr0300ds18b20.h

--- opnenag_dfr0300ds18b20.h
+++ opnenag_dfr0300ds18b20.h
@@ -14,7 +14,7 @@
  #include <DallasTemperature.h>
  
  
- class Dfr0300_Ds18b20: public Module{
+ class Dfr0300Ds18b20: public Module{
   public:
     Dfr0300_Ds18b20(int ec_pin, wt_pin);
     void begin();
@@ -22,7 +22,9 @@
     bool get_water_electrical_conductivity(std_msgs::Float32 &msg);
     bool get_water_temperature(std_msgs::Float32 &msg);
     
-   int wt_pin;
+    int _wt_pin;
+    int _ec_pin;
+  
   private:
     float getWEC();
     float _water_electrical_conductivity;
@@ -31,11 +33,13 @@
     float _ec_calibration_offset;
     
     float getWT();
-    OneWire _oneWire;
-    DeviceAddress _address;
-    DallasTemperature _sensors;
+    //OneWire _oneWire;
+    //DeviceAddress _address;
+    //DallasTemperature _sensors;
     bool _send_water_temperature;
     float _water_temperature;
     bool _waiting_for_conversion;
     
+    unit32_t _time_of_last_query; 
     const static uint32_t _min_update_interval = 2500;
+ }
